87b0d481dfe84a6b622757296638bef58cdc10a4
Your Name <you@example.com>
2024-07-04 23:57:21 +0530

    5th commit from intelij added new files

---
+++ src/main/java/Collections_Framework/Note.java
@@ -0,0 +1,10 @@
+package Collections_Framework;
+
+public class Note
+{
+    /**Collections:- It is a single unit or object  which can store multiple data
+     * framework:- Represents the library
+     * COLLECTION FRAMEWORK: - It is a set of pre-defined Classes & Interfaces which is used to store multiple Data
+     * there two types main packages 1. Java.util.Collection  &  2. Java.util.Map*/
+
+}
